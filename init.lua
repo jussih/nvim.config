4444558563c8755d08de9963a58adf7071f29b46
Jussi Heikkilä <jussih@gmail.com>
2024-11-08 10:19:24 +0200

    Don't use treesitter indent for python

--- init.lua
+++ init.lua
@@ -710,6 +710,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        -- jedi_language_server = {},
         -- Third party python-lsp-server plugins have to be managed manually
         -- eg. :PylspInstall pylsp-mypy
         -- pylsp = {},
@@ -1031,9 +1032,9 @@ require('lazy').setup({
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        additional_vim_regex_highlighting = { 'ruby', 'python' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { 'ruby', 'python' } },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:

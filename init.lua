305a3282a7cb972154960710567ac3dd9af2e34f
Jussi Heikkilä <jussih@gmail.com>
2025-01-04 16:36:02 +0200

    Enable clangd for C/C++

--- init.lua
+++ init.lua
@@ -699,7 +699,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         pyright = {},
         -- jedi_language_server = {},

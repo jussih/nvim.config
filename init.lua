5458db7d41443805e00cf5f4422e19d89b67eef6
Jussi Heikkilä <jussih@gmail.com>
2025-04-26 18:49:57 +0300

    Custom formatter for prettier via npx for markdown

--- init.lua
+++ init.lua
@@ -838,7 +838,7 @@ require('lazy').setup({
         javascript = { "prettier" },
         typescript = { "prettier" },
         json = { "prettier", "jq", stop_after_first = true },
-        markdown = { "prettier" },
+        markdown = { "prettier_npx" },
         html = { "prettier" },
         css = { "prettier" },
         sass = { "prettier" },
